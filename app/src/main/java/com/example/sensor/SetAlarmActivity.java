5350d0eea5d3ec704c3be43dcb2284a3afd829b3
sinatav <alirezatavakkoli@ymail.com>
2020-05-04 17:23:04 +0430

    what am i doing?

--- app/src/main/java/com/example/sensor/SetAlarmActivity.java
+++ app/src/main/java/com/example/sensor/SetAlarmActivity.java
@@ -35,73 +35,55 @@ public class SetAlarmActivity extends AppCompatActivity implements AdapterView.O
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_set);
 
-        // Initialize alarm manager, time picker, text update
         final AlarmManager alarm_manager = (AlarmManager) getSystemService(ALARM_SERVICE);
         final TimePicker alarm_timepicker = (TimePicker) findViewById(R.id.timePicker);
         @SuppressLint("CutPasteId") final TextView alarm_state = (TextView) findViewById(R.id.alarm_state);
 
-        // Create calendar instance
         final Calendar calendar = Calendar.getInstance();
 
-        // Create intent for AlarmReceiver class, send only once
         final Intent my_intent = new Intent(SetAlarmActivity.this, AlarmReceiver.class);
 
-        // Create spinner in main UI and corresponding ArrayAdapter
         @SuppressLint("CutPasteId") Spinner spinner = (Spinner) findViewById(R.id.spinner);
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource
                 (this, R.array.stepbrothers_array, android.R.layout.simple_spinner_item);
-        // Specify layout used for option list
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        // Apply adapter to spinner
         spinner.setAdapter(adapter);
-        // Set onClickListener for spinner
         spinner.setOnItemSelectedListener(this);
 
 
-        // Initialize start button and create onClickListener to start alarm
         @SuppressLint("CutPasteId") Button alarm_on = (Button) findViewById(R.id.alarm_on);
         alarm_on.setOnClickListener(new View.OnClickListener() {
             @SuppressLint("SetTextI18n")
             @Override
             public void onClick(View view) {
-                // Set calendar based on user input
                 calendar.set(Calendar.HOUR_OF_DAY, alarm_timepicker.getHour());
                 calendar.set(Calendar.MINUTE, alarm_timepicker.getMinute());
 
-                // Retrieve int values of hour and minute
                 int hour = alarm_timepicker.getHour();
                 int minute = alarm_timepicker.getMinute();
 
-                // Convert ints into strings
                 String hour_string = String.valueOf(hour);
                 String minute_string = String.valueOf(minute);
 
-                // Handles to format time data
                 if(hour > 12) hour_string = String.valueOf(hour - 12);
                 if(minute < 10) minute_string = "0" + String.valueOf(minute);
 
                 alarm_state.setText("Alarm set to: " + hour_string + ":" + minute_string);
 
-                // Put extra string into my_intent, indicates on button pressed
                 my_intent.putExtra("extra", "alarm on");
 
-                // Input extra long into my_intent
-                // Requests specific value from spinner
                 my_intent.putExtra("sound_choice", sound_select);
 
 
-                // Pending intent to delay intent until specific calendar time
                 pending_intent = PendingIntent.getBroadcast
                         (SetAlarmActivity.this, 0, my_intent, PendingIntent.FLAG_UPDATE_CURRENT);
 
-                // Set alarm manager
                 assert alarm_manager != null;
                 alarm_manager.set(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(), pending_intent);
 
             }
         });
 
-        // Initialize stop button and create onClick listener to stop alarm
         @SuppressLint("CutPasteId") Button alarm_off = (Button) findViewById(R.id.alarm_off);
         alarm_off.setOnClickListener(new View.OnClickListener() {
             @SuppressLint("SetTextI18n")
@@ -109,17 +91,13 @@ public class SetAlarmActivity extends AppCompatActivity implements AdapterView.O
             public void onClick(View view) {
                 alarm_state.setText("Alarm Off!");
 
-                // Cancel alarm
                 assert alarm_manager != null;
                 alarm_manager.cancel(pending_intent);
 
-                // Put extra string into my_intent, indicates off button pressed
                 my_intent.putExtra("extra", "alarm off");
 
-                // Also input extra long for alarm off to prevent null pointer exception
                 my_intent.putExtra("sound_choice", sound_select);
 
-                // Stop ringtone
                 sendBroadcast(my_intent);
             }
         });
@@ -127,8 +105,6 @@ public class SetAlarmActivity extends AppCompatActivity implements AdapterView.O
 
     @Override
     public void onItemSelected(AdapterView<?> adapterView, View view, int position, long id) {
-        // Output id which user selected
-        //Toast.makeText(parent.getContext(), "Spinner item is " + id, Toast.LENGTH_SHORT).show();
         sound_select = (int) id;
     }
 
